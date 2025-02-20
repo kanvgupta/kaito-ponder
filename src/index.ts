b6b375169925f5ee5f6aadf8e721ac3d47abf4f1
kanvgupta <85568419+kanvgupta@users.noreply.github.com>
2025-02-20 18:03:20 +0530

    removed log

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import {
 } from "ponder:schema";
 
 ponder.on("ERC20:Transfer", async ({ event, context }) => {
-  console.log(event.args.from, event.args.to, event.args.amount);
   if (event.args.from.toLowerCase() === "0xeB7d383B0c77EA0Bed28b42D0C288F9071Bd8a7a".toLowerCase()) {
     await context.db
       .insert(account)
